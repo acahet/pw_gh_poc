a2a62ac49e3d0c46589895b05343f60e275053ea
Anderson Cahet <anderson.cahet@gmail.com>
2024-10-16 17:33:08 +0200

    update

---
+++ tests/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(({ page }) => page.goto("https://playwright.dev/"));
+
+test("should succeed", async ({ page }) => {
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("should fail", async ({ page }) => {
+  await expect(page).not.toHaveTitle(/Playwright/);
+});
+
+test("should be flaky", async ({ page }) => {
+  if (Math.random() > 0.5) {
+    await expect(page).toHaveTitle(/Playwright/);
+  } else {
+    await expect(page).not.toHaveTitle(/Playwright/);
+  }
+});
