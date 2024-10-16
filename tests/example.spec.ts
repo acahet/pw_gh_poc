04bdd0b7598edba2a89bab404c00939f15356e4d
Anderson Cahet <anderson.cahet@gmail.com>
2024-10-16 17:37:36 +0200

    up

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,18 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(({ page }) => page.goto("https://playwright.dev/"));
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
-test("should succeed", async ({ page }) => {
+  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("should fail", async ({ page }) => {
-  await expect(page).not.toHaveTitle(/Playwright/);
-});
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
-test("should be flaky", async ({ page }) => {
-  if (Math.random() > 0.5) {
-    await expect(page).toHaveTitle(/Playwright/);
-  } else {
-    await expect(page).not.toHaveTitle(/Playwright/);
-  }
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole("heading", { name: "Insllation" })).toBeVisible();
 });
